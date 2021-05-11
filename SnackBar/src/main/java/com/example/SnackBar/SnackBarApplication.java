e69caf2e4a807d46a5275260c3a079551a62fdbe
Mohammed Hafiz <65387738+Hafiz408@users.noreply.github.com>
2021-05-11 18:09:22 +0530

    Add files via upload

---
+++ SnackBar/src/main/java/com/example/SnackBar/SnackBarApplication.java
@@ -0,0 +1,16 @@
+package com.example.SnackBar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
+public class SnackBarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnackBarApplication.class, args);
+	}
+
+}
---
+++ SnackBar/src/test/java/com/example/SnackBar/SnackBarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SnackBar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnackBarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
